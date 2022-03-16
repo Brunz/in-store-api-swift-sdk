bc3ad33810324f5fb4a13790097e388850dc9654
Emanuele Cionini <emanuele.cionini@gmail.com>
2022-03-16 16:45:13 +0100

    Initial commit SPM Branch

---
+++ Sources/SatispayInStore/Networking/Protocol/Models/StartedRequest.swift
@@ -0,0 +1,50 @@
+//
+//  StartedRequest.swift
+//  SatispayInStore
+//
+//  Created by Pierluigi D'Andrea on 09/10/17.
+//  Copyright © 2017 Satispay. All rights reserved.
+//
+
+import CoreLocation
+import Foundation
+#if os(iOS)
+import UIKit
+#endif
+
+public struct StartedRequest: Encodable {
+
+    let udid: String?
+    let language: String
+    let deviceInfo: String
+    let appVersion: String
+
+    #if os(macOS)
+    private let os = "macOS"
+    private let osVersion: String = ProcessInfo.processInfo.operatingSystemVersionString
+    #else
+    private let os: String = UIDevice.current.systemName
+    private let osVersion: String = UIDevice.current.systemVersion
+    #endif
+
+    enum CodingKeys: String, CodingKey {
+
+        case udid
+        case language
+        case deviceInfo = "device_model"
+        case os = "os_app"
+        case osVersion = "os_version"
+        case appVersion = "app_version"
+
+    }
+
+    public init(udid: String?, language: String, deviceInfo: String, appVersion: String) {
+
+        self.language = language
+        self.udid = udid
+        self.deviceInfo = deviceInfo
+        self.appVersion = appVersion
+
+    }
+
+}
